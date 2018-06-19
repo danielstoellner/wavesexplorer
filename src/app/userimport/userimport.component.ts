d46c849005a824067d00b6bcb686754663342c6a
Stefan <stefan_rechberger@gmx.at>
2018-06-19 12:10:12 +0200

    User-Import, Gruppenzuweisung

--- src/app/userimport/userimport.component.ts
+++ src/app/userimport/userimport.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx';
 import {BackendApiService} from '../common/backend-api.service';
 import {User} from '../users/user';
-import {Group} from "../groups/group";
+import {Group} from '../groups/group';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-userimport',
@@ -11,23 +12,35 @@ import {Group} from "../groups/group";
 })
 export class UserimportComponent implements OnInit {
   data: any;
-  groups: Group[]= [];
+  availablegroups: Group[] = [];
+  squadgroup: Group;
+  squadgroups: Group[] = [];
+  availableusers: User[] = [];
 
-  constructor(private backendApiService: BackendApiService) {}
+  constructor(
+    private backendApiService: BackendApiService,
+    private location: Location,
+  ) {}
 
   ngOnInit() {
     this.getGroups();
+    this.getUsers();
   }
   async getGroups() {
-    console.log("++++++++++++++++++++++++++++++++++")
     const result: Group[] = await this.backendApiService.getGroups().toPromise();
-    this.groups = result;
-    console.log(this.groups);
-    console.log(this.groups.length)
+    this.availablegroups = result;
+  }
+
+  async getUsers() {
+    const result: User[] = await this.backendApiService.getUsers().toPromise();
+    this.availableusers = result;
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
   onFileChange(evt: any) {
-
     /* wire up file reader */
     const target: DataTransfer = <DataTransfer>(evt.target);
     if (target.files.length !== 1) { throw new Error('Cannot use multiple files'); }
@@ -46,23 +59,49 @@ export class UserimportComponent implements OnInit {
        * Es wird für jede Zeile im Excel ein neuer User angelegt
        */
       this.data = XLSX.utils.sheet_to_json(ws, {header: 1});
-      console.log(this.data);
 
-      for (var i = 1; i < this.data.length; i++)
-      {
+      for (var i = 1; i < this.data.length; i++) {
         var newuser = new User();
         newuser.username = this.data[i][0];
         newuser.givenname = this.data[i][1];
         newuser.surename = this.data[i][2];
         newuser.address = this.data[i][3];
 
+        // insert new user
         this.backendApiService.addUser(newuser)
-        .subscribe(user => {});
-      }
+         .subscribe(user => {});
+        this.getUsers();
+        console.log(this.availableusers);
+        // find group in available groups
+        var groupname = this.data[i][4];
 
+        for(var j = 0; j < this.availablegroups.length; j++) {
+          if (groupname === this.availablegroups[j].name) {
+            this.squadgroup = this.availablegroups[j];
+            this.squadgroups[0] = this.squadgroup;
+          }
+        }
+        // if groupname found in availableGroups then update user
+        if (this.squadgroups[0] != null) {
+          newuser.squads = this.squadgroups;
+          this.backendApiService.getUsers().toPromise().then((res) => {
+            console.log(res);
+            console.log(this.availableusers);
+            for(var k = 0; k < this.availableusers.length; k++) {
+              if (newuser.address === this.availableusers[k].address) {
+                newuser.id = this.availableusers[k].id;
+              }
+            }
+            console.log(this.availableusers);
+            console.log("User-ID: " + newuser.id);
+            this.backendApiService.updateUser(newuser)
+              .subscribe(() => {});
+            this.squadgroups = [];
+            this.squadgroup = null;
+          })
+        }
+      }
     };
     reader.readAsBinaryString(target.files[0]);
   }
-
-
 }
