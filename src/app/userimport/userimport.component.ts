3d5ac54d2ae1b1b0d4ac97723178f8533db8acc7
Stefan <stefan_rechberger@gmx.at>
2018-06-21 09:49:53 +0200

    User-Import, Gruppenzuweisung

--- src/app/userimport/userimport.component.ts
+++ src/app/userimport/userimport.component.ts
@@ -4,7 +4,6 @@ import {BackendApiService} from '../common/backend-api.service';
 import {User} from '../users/user';
 import {Group} from '../groups/group';
 import {Location} from '@angular/common';
-import {isUndefined} from "util";
 
 @Component({
   selector: 'app-userimport',
@@ -14,12 +13,11 @@ import {isUndefined} from "util";
 export class UserimportComponent implements OnInit {
   data: any;
   availablegroups: Group[];
-  squadgroup: Group;
-  squadgroups: Group[] = [];
   availableusers: User[] = [];
   cols: any[];
   users: User[];
   selectedUser: User;
+  messages: string[] = [];
 
   constructor(
     private backendApiService: BackendApiService,
@@ -84,119 +82,38 @@ export class UserimportComponent implements OnInit {
         newuser.surename = this.data[i][2];
         newuser.address = this.data[i][3];
         newuser.squads = [];
-        if (!(this.data[i][4] === undefined)) {
-          console.log(this.data[i][4]);
           var group: Group = new Group();
           group.name = this.data[i][4];
           newuser.squads.push(group);
-        }
         this.users.push(newuser);
       }
     };
     reader.readAsBinaryString(target.files[0]);
   }
 
-  async onFileChange2(evt: any) {
-    /* wire up file reader */
-    const target: DataTransfer = <DataTransfer>(evt.target);
-    if (target.files.length !== 1) {
-      throw new Error('Cannot use multiple files');
-    }
-    const reader: FileReader = new FileReader();
-    reader.onload = (e: any) => {
-      /* read workbook */
-      const bstr: string = e.target.result;
-      const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
-
-      /* grab first sheet */
-      const wsname: string = wb.SheetNames[0];
-      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-
-      /**
-       * Daten aus dem Excel sheet auslesen und speichern
-       * Es wird für jede Zeile im Excel ein neuer User angelegt
-       */
-      this.data = XLSX.utils.sheet_to_json(ws, {header: 1});
-
-      for (var i = 1; i < this.data.length; i++) {
-        var newuser = new User();
-        newuser.username = this.data[i][0];
-        newuser.givenname = this.data[i][1];
-        newuser.surename = this.data[i][2];
-        newuser.address = this.data[i][3];
-        
-        // insert new user
-        this.backendApiService.addUser(newuser).toPromise();
-        this.getUsers();
-
-        console.log(this.availableusers);
-        // find group in available groups
-        var groupname = this.data[i][4];
-
-        for (var j = 0; j < this.availablegroups.length; j++) {
-          if (groupname === this.availablegroups[j].name) {
-            this.squadgroup = this.availablegroups[j];
-            this.squadgroups[0] = this.squadgroup;
-          }
-        }
-        // if groupname found in availableGroups then update user
-        if (this.squadgroups[0] != null) {
-          newuser.squads = this.squadgroups;
-          this.backendApiService.getUsers().toPromise().then((res) => {
-            for (var k = 0; k < this.availableusers.length; k++) {
-              if (newuser.address === this.availableusers[k].address) {
-                newuser.id = this.availableusers[k].id;
-              }
-            }
-            console.log(this.availableusers);
-            console.log("User-ID: " + newuser.id);
-            this.backendApiService.updateUser(newuser)
-              .subscribe(() => {
-              });
-            this.squadgroups = [];
-            this.squadgroup = null;
-          })
-        }
-      }
-    };
-    reader.readAsBinaryString(target.files[0]);
-  }
-
   addNewUser(): void {
+    var counter = 0;
+    var groupname: string;
     this.users.forEach(user => {
       var exists: boolean = false;
       this.availablegroups.forEach(group => {
-        if (user.squads[0] === undefined){
-          exists = true;
-        } else if (user.squads[0].name === group.name) {
-          console.log(group);
+      if (user.squads[0].name === group.name) {
           user.squads[0] = group;
           exists = true;
         }
+        groupname = user.squads[0].name;
       })
 
-      if (exists) {
+      if (exists || groupname === undefined) {
         //group exist
         this.backendApiService.addUser(user).toPromise();
       } else {
         // group does not exist
-        this.addGroup(user.squads[0].name);
+        this.messages[counter] = "Group does not exist! User: " + user.surename + " | Group: " + groupname;
+        counter = counter + 1;
         this.backendApiService.addUser(user).toPromise();
       }
-      this.goBack();
     })
+    this.messages[counter] = "DATA IMPORTED";
   }
-
-  addGroup(name: string): void {
-    name = name.trim();
-    var currency = 'Waves';
-
-    if (!name) {
-      return;
-    }
-    var result = this.backendApiService.addGroup({name, currency} as Group)
-      .subscribe(group => {
-      this.availablegroups.push(group);
-    })
-  };
 }
